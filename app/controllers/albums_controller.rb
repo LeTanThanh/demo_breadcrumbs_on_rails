df4091ee75e8979d676823ded35ddc90172c1126
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 10:52:17 +0700

    refector_add_breadcrum_in_albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,19 +2,15 @@ class AlbumsController < ApplicationController
   before_action :load_artist, only: :index
   before_action :load_album, only: :show
 
+  before_action :add_breadcrumb_artist_path
+  before_action :add_breadcrumb_albums_path
+  before_action :add_breadcrumb_album_path, only: :show
+
   def index
     @albums = @artist.albums
-    
-    add_breadcrumb "Artist #{@artist.name}", "/artists/#{@artist.id}"
-    add_breadcrumb "Albums", "/albums?artist_id=#{@artist.id}"
   end
 
   def show
-    @artist = @album.artist
-  
-    add_breadcrumb "Artist #{@artist.name}", "/artists/#{@artist.id}"
-    add_breadcrumb "Albums", "/albums?artist_id=#{@artist.id}"
-    add_breadcrumb "Album #{@album.name}", :album_path
   end
 
   private
@@ -28,5 +24,19 @@ class AlbumsController < ApplicationController
   def load_album
     @album = Album.find_by id: params[:id]
     redirect_back(fallback_location: root_url) unless @album
+
+    @artist = @album.artist
+  end
+
+  def add_breadcrumb_artist_path
+    add_breadcrumb "Artist #{@artist.name}", "/artists/#{@artist.id}"
+  end
+
+  def add_breadcrumb_albums_path
+    add_breadcrumb "Albums", "/albums?artist_id=#{@artist.id}"
+  end
+
+  def add_breadcrumb_album_path
+    add_breadcrumb "Album #{@album.name}", :album_path
   end
 end
