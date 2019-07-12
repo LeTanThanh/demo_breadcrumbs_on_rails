f941b7a9f410f289cee964080ce798c1633bc907
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 08:37:55 +0700

    add_albums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_011556) do
+ActiveRecord::Schema.define(version: 2019_07_12_013553) do
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "artist_id"
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 10.times do
   artist = Artist.create name: FFaker::Music.artist, description: FFaker::Lorem.sentence
+
+  10.times do
+    artist.albums.create name: FFaker::Music.album, description: FFaker::Lorem.sentence
+  end
 end
