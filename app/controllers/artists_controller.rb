82ed5745de8d526380b22e02d52cf2aa2a4d4f9e
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-12 08:33:40 +0700

    add_artists#show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,18 @@
 class ArtistsController < ApplicationController
+  before_action :load_artist, only: :show
+
   def index
     @artists = Artist.all
+  end
+
+  def show
+  end
+
+  private
+
+  def load_artist
+    @artist = Artist.find_by id: params[:id]
+
+    redirect_to root_url unless @artist
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "artists#index"
+
+  resources :artists, only: :show
 end
